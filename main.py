28996f91d5775d7104ef284dd19581d2d164c3b6
L1ttlY <50800940+L1ttlY@users.noreply.github.com>
2019-05-25 16:50:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def process_file(file_name):
     data = []
     with open(file_name) as f:
         for line in f:
-            parts = line.split(',')
+            parts = line.split()
             data.append(parts)
           #  row = [
            #     parts[0], parts[1], parts[2]
@@ -11,5 +11,9 @@ def process_file(file_name):
     return data
 
 
-data = process_file('C:\\Users\\titov\\Desktop\\Project\\NoteData.csv')
+data = process_file('NoteData.csv')
 print(data)
+
+
+# def telegram_bot():
+# There should be code for telegram bot :p(work in progress)
