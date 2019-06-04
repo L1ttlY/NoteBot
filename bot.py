f4bb3679658ba369941975be52ba268f3de070d0
L1ttlY <50800940+L1ttlY@users.noreply.github.com>
2019-06-04 17:41:01 +0300

    bot v1.1

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+import time
 
 bot_token = '764198186:AAGPu1-Bc8gVQDmL1ZO0sTgHkV8bLmjv2DU'
 
@@ -7,16 +8,16 @@ bot = telebot.TeleBot(token=bot_token)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Welcome!')
+    bot.reply_to(message, 'Hi! I am NoteBot - telegram bot that can send notes for musical instruments =)')
 
 
 @bot.message_handler(commands=['help'])
-def send_welcome(message):
-    bot.reply_to(message, 'To use this bot, send it a username')
+def send_help(message):
+    bot.reply_to(message, 'All commands: /features, /start, /help')
 
 
 @bot.message_handler(commands=['features'])
-def send_welcome(message):
+def send_features(message):
     bot.reply_to(message, 'In the future i would be able to send notes')
 
 bot.polling()
